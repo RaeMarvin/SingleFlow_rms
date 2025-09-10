403e84112db7d6f3cda2987dd5cf06d88cb9c59d
Rae-at-672468481530 <raemarvin@googlemail.com>
2025-09-10 08:54:46 +0100

    Resolve the icon not showing

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import favicon from './assets/logo.png';
+
+// Set the favicon dynamically to ensure it's included in the build
+const faviconLink = document.querySelector("link[rel~='icon']") as HTMLLinkElement;
+if (faviconLink) {
+  faviconLink.href = favicon;
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
